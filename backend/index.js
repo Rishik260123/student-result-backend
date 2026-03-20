fe455cfd76eff595de35675c89ef02e59faad6ba
Rishik260123 <rishikbhardwaj0708@gmail.com>
2026-03-20 16:18:33 +0530

    force render port usage

--- backend/index.js
+++ backend/index.js
@@ -78,5 +78,6 @@ app.use((err, _req, res, _next) => {
 const PORT = process.env.PORT || 5000
 
 app.listen(PORT, '0.0.0.0', () => {
+  console.log('🔥 PORT VALUE:', process.env.PORT)
   console.log(`🚀 Server running on port ${PORT}`)
 })
